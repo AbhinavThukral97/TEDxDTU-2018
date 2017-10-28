a7f4decd9389ccdca53882fb7a56b9b16b309646
Abhinav Thukral <thukral.abhinav@gmail.com>
2017-10-28 14:46:57 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-
-	var w = $(window).width()/2;
-
+	var w = $(window).width()/2;	
+	var w1 = w/75;
+	var w2 = w/40;
 	$('.play').mousemove(function(e){
-		$('.building').css('left', (w - e.clientX)/75 + 'px');
-		$('.foreground').css('left', (w - e.clientX)/40 + 'px');
+		$('.building').css('left', w1 - e.clientX/75 + 'px');
+		$('.foreground').css('left', w2 - e.clientX/40 + 'px');
 	});
-
 });
